1ae341816548b143fc9c650ac7ced42486e04c87
bdc65469 <bdc65469@educastur.es>
2025-02-17 12:35:20 +0100

    Cambios en el controlador de mensajes

--- tarea7dwesGeraldSara/src/main/java/com/geraldSara/tarea7dwesGeraldSara/controladores/ControladorMensajes.java
+++ tarea7dwesGeraldSara/src/main/java/com/geraldSara/tarea7dwesGeraldSara/controladores/ControladorMensajes.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -15,13 +15,26 @@ import com.geraldSara.tarea7dwesGeraldSara.modelo.Mensaje;
 import com.geraldSara.tarea7dwesGeraldSara.modelo.Persona;
 import com.geraldSara.tarea7dwesGeraldSara.servicios.ServiciosFactory;
 
-@Controller
-public class ControladorVerMensajesUsuario {
+import jakarta.servlet.http.HttpSession;
 
+@Controller
+@RequestMapping("/mensajes")
+public class ControladorMensajes {
+	
 	@Autowired
 	ServiciosFactory factory;
 
-	@GetMapping("/vermensajesusuario")
+	@GetMapping("/menu")
+	public String menumensajes(HttpSession session) {
+		String usuario = (String) session.getAttribute("usuario");
+		if (usuario == null) {
+
+			return "redirect:/login";
+		}
+		return "menumensajes";
+	}
+	
+	@GetMapping("/usuarios")
 	public String listadoUsuarios(Model model) {
 
 		List<Credenciales> credenciales = factory.getServiciosCredenciales().listaUsuario();
@@ -31,8 +44,8 @@ public class ControladorVerMensajesUsuario {
 		return "vermensajesusuario";
 	}
 
-	@PostMapping("/vermensajes")
-	public String verMensajes(@RequestParam String usuario, Model model, RedirectAttributes redirectAttributes) {
+	@GetMapping("/mensajesUsuario")
+	public String verMensajes(@RequestParam("usuario") String usuario, Model model, RedirectAttributes redirectAttributes) {
 
 		Persona p = factory.getServiciosPersona().obtenerPersonaPorUsuario(usuario);
 		List<Mensaje> mensajes = factory.getServiciosMensaje().obtenerMensajesPorPersona(p.getId());
